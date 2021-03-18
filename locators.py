a0db2bc40a626906ca7f6b1b8aaaf101dc6b1587
patryksla <patryk.slawski@wp.pl>
2021-03-18 23:43:40 +0100

    create locators and home_page.py in pages

---
+++ locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+class HomePageLocators():
+    CLOSE_NEWSLETTER_BTN = (By.XPATH, '//div [@class="popup-window-close-button"][@popup-window-id="3"]')
+    ZALOGUJ_BTN = (By.XPATH, '//*[@id="layoutPortletElement_359"]/header/div/div[3]/ul/li[2]/a/span')
