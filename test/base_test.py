c8087cf2e8fceb96c9192848c3715a9f89b25b5a
patryksla <patryk.slawski@wp.pl>
2021-05-05 15:40:29 +0200

    create base_test and registration_page_test

---
+++ test/base_test.py
@@ -0,0 +1,12 @@
+import unittest
+from selenium import webdriver
+
+class BaseTest(unittest.TestCase):
+    def setUp(self):
+        print("setUp z BaseTest")
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.ambersport.pl/")
+        self.driver.maximize_window()
+
+    def tearDown(self):
+        self.driver.quit()
